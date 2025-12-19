07d17202a61cd33077c231b86374d48db8c80f14
Michał Mościcki <michal.moscicki@faktura.pl>
2025-12-19 13:33:00 +0100

    scetch

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+public class Main {
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Podaj port");
+        Scanner scanner = new Scanner(System.in);
+        String ipAddress = scanner.nextLine();
+
+        //funkcja, która sprawdza, czy adress ip jest poprawny - jak tak, nic nie robi, jak nie, przerywa program i wypisuje błąd
+
+        Map<String, String> result = new HashMap<>();
+
+        //todo potem przerobić to na wiele wątków
+        for (int i = 1; i <= 1024; i++) {
+            //nawiąż połączenie
+            //jak się udało, to nic nie rób i zamknij połączenie
+            //jak się nie udało, to dodaj do mapy:
+            result.get(i, powód nie udania się);
         }
+
+
+        System.out.println("Wynik:");
+        System.out.println(result.toString());
     }
 }
